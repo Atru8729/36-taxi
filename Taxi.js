98f7992b49061ae8d9ccd74e57ee057fb8e8e562
Atru8729 <a.truskauskas1@gmail.com>
2021-08-03 21:08:43 +0300

    tarpinis

--- Taxi.js
+++ Taxi.js
@@ -1,5 +1,23 @@
 class Taxi {
+    constructor(name) {
+        this.name = name;
+        this.distance = distance;
+        this.price = price;
 
+    }
+    intro() {
+        console.log(`Sveiki, as ${this.name} ir kilometro kaina su manimi yra 1.00 Eur.`);
+    }
+    updatePrice(price) {
+        console.log(`${this.name} nustate nauja kilometro kaina - ${price.toFixed(2)} Eur.`);
+    }
+    ride(distance) {
+        console.log(distance);
+    }
+    stats() {
+
+        console.log(`${this.name} nuvaziavo ${this.distance} km ir uzdirbo ${(this.distance * this.price).toFixed(2)} Eur.`);
+    }
 }
 
 module.exports = Taxi;
